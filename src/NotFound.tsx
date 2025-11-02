68569a3fffc4b75ff0ca129edbf573d2e7feddc5
Eric Xiao <ericxiao0000@gmail.com>
2025-11-02 08:12:19 -0800

    Use A component instead of a for links

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -1,11 +1,13 @@
+import { A } from "@solidjs/router"
+
 const NotFound = () => {
   return (
     <div class="min-h-screen p-10">
       <main class="max-w-[1200px] m-auto">
         <h1 class="text-3xl">404 page not found</h1>
-        <a href="/" class="underline text-blue-600">
+        <A href="/" class="underline text-blue-600">
           click here to return
-        </a>
+        </A>
       </main>
     </div>
   )
