6d13472964e716f3d38d0ad54182f20a0c12ed24
Eric Xiao <ericxiao0000@gmail.com>
2025-11-01 22:55:45 -0700

    Add solidjs starter code

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div class="min-h-screen p-10">
+      <main class="max-w-[1200px] m-auto">
+        <h1 class="text-3xl">duckhunt</h1>
+        this app does cool things like scavenger hunt
+        <br />
+        yip
+      </main>
+    </div>
+  )
+}
+
+export default App
