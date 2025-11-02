bc0f7972432bc2e2f77f7ee63f96534426e76264
Eric Xiao <ericxiao0000@gmail.com>
2025-11-02 09:07:51 -0800

    Add welcome page with input for event code

--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -1,12 +1,36 @@
+import { createSignal } from "solid-js"
+
 const Welcome = () => {
+  const [code, setCode] = createSignal("")
+
   return (
-    <main class="h-screen flex flex-col gap-1 justify-center items-center">
-      <h1>DuckHunt</h1>
-      <input
-        type="text"
-        placeholder="Enter your event code"
-        class="text-center"
-      />
+    <main class="h-screen flex justify-center items-center text-center">
+      <form
+        class="flex flex-col gap-1 items-center"
+        onSubmit={(e) => {
+          e.preventDefault()
+          alert(code())
+        }}
+      >
+        <h1>DuckHunt</h1>
+        <input
+          type="text"
+          value={code()}
+          onInput={(e) => {
+            setCode(e.target.value)
+          }}
+          placeholder="Enter your event code"
+          class="text-center"
+        />
+        <button
+          type="submit"
+          disabled={code() === ""}
+          class="disabled:text-gray-500 disabled:outline-gray-500 rounded outline-black outline-1
+                 w-min px-2 not-disabled:hover:bg-gray-300 not-disabled:cursor-pointer"
+        >
+          Join
+        </button>
+      </form>
     </main>
   )
 }
